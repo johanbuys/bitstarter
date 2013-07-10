8639aa95b65d50cdf96a6054837a0cb277fcc467
johanbuys <johan@appbase.co.za>
2013-07-10 12:46:24 +0000

    Read from index.html

--- web.js
+++ web.js
@@ -2,8 +2,10 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  var file = fs.readFileSync('./index.html','utf8');
+  response.send(file);
 });
 
 var port = process.env.PORT || 5000;
